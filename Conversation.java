d3afc4f0db22221583289b6bf729c65c6a3c972b
tajhini <thbrown@smith.edu>
2023-02-15 21:56:55 -0500

    Prototype 1

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,141 @@
-class Conversation {
+import java.util.Scanner;
+import java.util.Arrays;
 
+
+/** A class that contains a method that creates a chat bot that interacts with a user for a specific amount of conversation rounds
+ * @author Tajhini Brown
+ */
+class Conversation {
+ /**
+ * Creates a chat bot that interacts with a user for a specific amount of conversation rounds
+ * @param String[] arguments, list
+ * @return returns nothing
+ */
   public static void main(String[] arguments) {
     // You will start the conversation here.
+      
+      String[] canned_words = {"Mmm-hm.", "Oh Okay", "Interesting", "That's cool"};
+      String[] pronoun_words = {"I", "me", "am", "you", "my", "your", "I'm"};
+      String[] mirror_words = {"You", "you", "are", "I", "your", "my", "You're"};
+      String[] transcript = {};
+      int min = 0;
+      int max = (canned_words.length -1);
+      
+      Scanner Input = new Scanner(System.in);
+      System.out.println("Insert the number of rounds of conversations.");
+      int convoNumber = Input.nextInt();
+
+      Scanner Input_1 = new Scanner(System.in);
+      System.out.println("Hello, what are you thinking about?");
+      String userInput = Input_1.nextLine();
+    
+      transcript = Arrays.copyOf(transcript, transcript.length + 1);
+      transcript[transcript.length - 1] = "Hello, what are you thinking about?";
+      transcript = Arrays.copyOf(transcript, transcript.length + 1);
+      transcript[transcript.length - 1] = userInput;
+
+      for ( int x = 0; x < (convoNumber-1); x++){
+
+                if (userInput.contains("I ") || userInput.contains("me") || userInput.contains("am") || userInput.contains("you") || userInput.contains("my") || userInput.contains("your") ||  userInput.contains("I'm")){
+                  
+                  String[] inputsplit = userInput.split(" ", 0);
+
+                  for (int i = 0; i < inputsplit.length; i++) {
+                    
+                        if (inputsplit[i].equals("I") ) {
+                          int word_placement = Arrays.asList(pronoun_words).indexOf("I");
+                          int placement_phrase = Arrays.asList(inputsplit).indexOf("I");
+                          String wrd_in_ref_phrase = mirror_words[word_placement];
+                          inputsplit[placement_phrase] = wrd_in_ref_phrase;
+
+                          if (inputsplit[i+1].equals("am") ) {
+                            int word_placement_1 = Arrays.asList(pronoun_words).indexOf("am");
+                            int placement_phrase_1 = Arrays.asList(inputsplit).indexOf("am");
+                            String wrd_in_ref_phrase_1 = mirror_words[word_placement_1];
+                            inputsplit[placement_phrase_1] = wrd_in_ref_phrase_1;
+                          }
+                        }
+                  
+                        if (inputsplit[i].equals("you") ) {
+                          int word_placement = Arrays.asList(pronoun_words).indexOf("you");
+                          int placement_phrase = Arrays.asList(inputsplit).indexOf("you");
+                          String wrd_in_ref_phrase = mirror_words[word_placement];
+                          inputsplit[placement_phrase] = wrd_in_ref_phrase;
+                        }
+                        if (inputsplit[i].equals("my") ) {
+                          int word_placement = Arrays.asList(pronoun_words).indexOf("my");
+                          int placement_phrase = Arrays.asList(inputsplit).indexOf("my");
+                          String wrd_in_ref_phrase = mirror_words[word_placement];
+                          inputsplit[placement_phrase] = wrd_in_ref_phrase;
+                        }
+                        if (inputsplit[i].equals("your") ) {
+                          int word_placement = Arrays.asList(pronoun_words).indexOf("your");
+                          int placement_phrase = Arrays.asList(inputsplit).indexOf("your");
+                          String wrd_in_ref_phrase = mirror_words[word_placement];
+                          inputsplit[placement_phrase] = wrd_in_ref_phrase;
+                        }
+                        if (inputsplit[i].equals("I'm") ) {
+                          int word_placement = Arrays.asList(pronoun_words).indexOf("I'm");
+                          int placement_phrase = Arrays.asList(inputsplit).indexOf("I'm");
+                          String wrd_in_ref_phrase = mirror_words[word_placement];
+                          inputsplit[placement_phrase] = wrd_in_ref_phrase;
+                        }
+                
+                  }
+                  String new_phrase = String.join(" ", inputsplit);
+                  String return_message = new_phrase + "?";
+                  transcript = Arrays.copyOf(transcript, transcript.length + 1);
+                  transcript[transcript.length - 1] = return_message;
+
+                  System.out.println(return_message);
+                  Scanner Input_2 = new Scanner(System.in);
+                  userInput = Input_2.nextLine();
+                  transcript = Arrays.copyOf(transcript, transcript.length + 1);
+                  transcript[transcript.length - 1] = userInput;
+                  
+                } else {
+                  int canned_index = (int)(Math.random()*(max-min+1)+min);
+                  String canned = canned_words[canned_index];
+                  System.out.println(canned);
+                  transcript = Arrays.copyOf(transcript, transcript.length + 1);
+                  transcript[transcript.length - 1] = canned;
+
+                  Scanner Input_3 = new Scanner(System.in);
+                  userInput = Input_3.nextLine();
+                  transcript = Arrays.copyOf(transcript, transcript.length + 1);
+                  transcript[transcript.length - 1] = userInput;
+                  
+                } 
+      }
+      int canned_index_1= (int)(Math.random()*(max-min+1)+min);
+      String final_statement = canned_words[canned_index_1];
+      System.out.println(final_statement);
+      transcript = Arrays.copyOf(transcript, transcript.length + 1);
+      transcript[transcript.length - 1] = final_statement;
+      System.out.println("Well...goodbye.");
+      transcript = Arrays.copyOf(transcript, transcript.length + 1);
+      transcript[transcript.length - 1] = "Well...goodbye.";
+
+      System.out.println("***TRANSCRIPT***");
+      int transcript_range = transcript.length;
+      for (int z = 0; z < transcript_range; ++z){
+        System.out.println(transcript[z]);
+        
+      }
+      
+    Input.close();
+    Input_1.close();
+    
+
+
+
+
+
+      
+
+
+
+
+
   }
 }
--- reflection.md
+++ reflection.md
@@ -1,7 +1,43 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+Hala
+Mira
+Musa
+Talking with my classmate helped me figur eout how to approach coding in java.
+
 - Which session(s) of TA / office hours did you attend?
+Monday 3-4 with Jordan
+Jordan helped me change the while loop I initally had in my function to a for loop which made coding the rest of this project easier.
 - What are your initial impressions of Java? 
+The syntax is so complicated. I think due to the fact that it is less similar to english and I'm less fmailiar with it, I can't conceptualise my ideas or implement them in Java quite yet which is what lead me to writing the code in python then translating it to python.
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+Python is way closer to english than Java so its harder to code in it for me personally.
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+I wrote down in comments the first small steps I wanted to take such as having a variable for the number of conversations I wanted to have and wrote that down in code. I tried to continues but I didnt have a very clear plan so thats what I did next.
+
+I started writing the program in python so that it would be easier to write in java since python is essentially usable pseudo code. 
+
+At first I tried doing everything in the while loop but then I decided to make two functions. One for canned responses and one for the mirror responses.
+
+I tried using "thinking" as a marker for where to change the mirror words but but the I realised that that would not work if the entered phrase did not have thinking in it.
+
+Created two lists of the mirror words and used if statements to iterate through them then combined the changed words at the end.
+
+My loop was going one extra iteration so I did convonumber - 1
+
+To print transcript, I appeneded the stuff I was ouputting to a list then used a for loop to print the list.
+
+Recommendation:
+Coding has been happening for a long time. Someone on the internet has had a similar problem to you and you can learn from what they did.
+
+Sources used:
+https://www.w3schools.com/java/default.asp
+https://www.geeksforgeeks.org/arrays-in-java/
+https://www.educative.io/answers/how-to-append-to-an-array-in-java
+https://www.geeksforgeeks.org/split-string-java-examples/
+https://stackoverflow.com/questions/1978933/a-quick-and-easy-way-to-join-array-elements-with-a-separator-the-opposite-of-sp
+https://www.javatpoint.com/how-to-generate-random-number-in-java
+https://www.tutorialspoint.com/java/util/scanner_close.htm
+https://www.freecodecamp.org/news/generate-random-numbers-java/
